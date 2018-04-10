1ef8aa5e5e3ecf451fc3d1f89c48e975ed9f3c4e
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-10 19:29:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  array_of_badge_messages = []
+  i = 0
+  while i < names.size
+    array_of_badge_messages.push("Hello, my name is #{names[i]}.")
+    i += 1
+  end
+  return array_of_badge_messages
+end
+
+def assign_rooms
+
